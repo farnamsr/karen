29e8014e452f88c68776a75c1e86d11f2c4f460d
Farhad <farhadnmr95@gmail.com>
2023-04-24 20:47:34 +0330

    Updated orders migrations

--- database/migrations/2023_04_01_161041_create_orders_table.php
+++ database/migrations/2023_04_01_161041_create_orders_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
             $table->bigInteger("user_id")->unsigned();
+            $table->tinyInteger("status");
             $table->timestamps();
         });
     }
--- database/migrations/2023_04_01_163332_create_order_details_table.php
+++ database/migrations/2023_04_01_163332_create_order_details_table.php
@@ -13,7 +13,10 @@ return new class extends Migration
     {
         Schema::create('order_details', function (Blueprint $table) {
             $table->id();
-            $table->string("color", 32);
+            $table->unsignedBigInteger("order_id");
+            $table->unsignedBigInteger("product_id");
+            $table->unsignedBigInteger("payable");
+            $table->string("color", 32)->nullable();
             $table->integer("count")->unsigned();
             $table->string("unit_price", 16);
             $table->timestamps();
